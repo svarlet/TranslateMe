f9a6ce3f1c8baa2ce152652187f74897a42a7104
Sebastien Varlet <svarlet@gsngames.com>
2016-12-29 19:23:05 +0000

    Big refactoring from a component architecture to plain functions.

--- Main.elm
+++ Main.elm
@@ -1,72 +1,116 @@
 module Main exposing (..)
 
 import Html exposing (Html, Attribute, div, input, text)
+import Http
 import Platform.Sub exposing (..)
-import Game
-import Bootstrap
+import CsvParser
+import Random
+import Random.List
+import Maybe
 
 -- MODEL
 
+type RemoteData e a
+    = NotRequested
+    | Loading
+    | Success a
+    | Failure e
+
+type alias WebData a =
+    RemoteData Http.Error a
+
+type alias Translation
+    = List String
+
+type alias Translations
+    = List Translation
+
+type SelectedTranslation
+    = NoSelectionYet
+    | Selection (Maybe Translation)
+
 type alias Model =
-    { gameModel : Game.Model
-    , bootstrapModel : Bootstrap.Model
-    , view : View
+    { translations : WebData Translations
+    , currentTranslation : SelectedTranslation
     }
 
 initialModel : Model
 initialModel =
-    Model Game.initialModel Bootstrap.initialModel Bootstrap
+    Model NotRequested NoSelectionYet
 
 init : ( Model, Cmd Msg )
 init =
-    ( initialModel, Cmd.map BootstrapMsg Bootstrap.downloadWords )
+    ( { initialModel | translations = Loading }
+    , loadTranslations
+    )
 
 -- UPDATE
 
 type Msg
-    = GameMsg Game.Msg
-    | BootstrapMsg Bootstrap.Msg
+    = LoadingComplete (Result Http.Error String)
+    | RandomTranslationPicked (Maybe (List String), Translations)
+
+loadTranslations : Cmd Msg
+loadTranslations =
+    "https://dl.dropboxusercontent.com/u/4800046/word-list.csv"
+        |> Http.getString
+        |> Http.send LoadingComplete
+
+pickRandomTranslation : Translations -> Cmd Msg
+pickRandomTranslation translations =
+    let
+        translationRandomGenerator = Random.List.choose translations
+    in
+        Random.generate (RandomTranslationPicked) translationRandomGenerator
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GameMsg gameMsg ->
+        LoadingComplete (Ok csvContent) ->
             let
-                (updatedModel, cmd) =
-                    Game.update gameMsg model.gameModel
+                translations = CsvParser.parseCsv csvContent
             in
-                ( { model | gameModel = updatedModel }, Cmd.map GameMsg cmd )
-        BootstrapMsg bootstrapMsg ->
-            let
-                (updatedModel, cmd) =
-                    Bootstrap.update bootstrapMsg model.bootstrapModel
-                view =
-                    case updatedModel.status of
-                        Bootstrap.Loaded ->
-                            Game
-                        _ ->
-                            Bootstrap
-            in
-                ( { model
-                      | bootstrapModel = updatedModel
-                      , view = view
-                  }
-                , Cmd.map BootstrapMsg cmd
+                ( { model | translations = Success translations }
+                , pickRandomTranslation translations
                 )
+        LoadingComplete (Err error) ->
+            ( { model | translations = Failure error }
+            , Cmd.none
+            )
+        RandomTranslationPicked (maybeTranslation, remainingTranslations) ->
+            ( { model
+                  | translations = Success remainingTranslations
+                  , currentTranslation = Selection maybeTranslation
+              }
+            , Cmd.none
+            )
 
 -- VIEW
 
-type View
-    = Bootstrap
-    | Game
+viewBootstrap : Html Msg
+viewBootstrap =
+    text "Bootstrapping..."
+
+viewTranslationExercise : Maybe Translation -> Html Msg
+viewTranslationExercise translation =
+    case translation of
+        Just aTranslation ->
+            let
+                englishWord = List.head aTranslation
+                frenchWords = List.tail aTranslation
+            in
+                div []
+                    [text <| (toString englishWord) ++ (toString frenchWords)]
+        Nothing ->
+            text "Sorry, I could not prepare another question for you. :-("
 
 view : Model -> Html Msg
 view model =
-    case model.view of
-        Bootstrap ->
-            Html.map BootstrapMsg (Bootstrap.view model.bootstrapModel)
-        Game ->
-            Html.map GameMsg (Game.view model.gameModel)
+    case model.currentTranslation of
+        NoSelectionYet ->
+            viewBootstrap
+        Selection maybeTranslation ->
+            viewTranslationExercise maybeTranslation
 
 -- MAIN
 
