261b6116d064a30137f82313ac4392cac8e79cfb
Sebastien Varlet <svarlet@gsngames.com>
2017-01-16 19:37:16 +0000

    Add unit and property based tests for the Score data type

---
+++ tests/Tests.elm
@@ -0,0 +1,39 @@
+module Tests exposing (..)
+
+import Test exposing (..)
+import Expect
+import Fuzz exposing (list, int, tuple, string, Fuzzer)
+import Types.Score as Score exposing (Score)
+
+
+all : Test
+all =
+    describe "Sample Test Suite"
+        [ scoreTests
+        ]
+
+
+scoreTests : Test
+scoreTests =
+    describe "Tests for the Score data type"
+        [ test "init returns a score of 0/0" <|
+            \() ->
+              Expect.equal (0, 0) Score.init
+        , fuzz scoreFuzzer "succeed increments both numbers" <|
+            \(x, y) ->
+              Expect.equal (x + 1, y + 1) <| Score.succeed (x, y)
+        , fuzz scoreFuzzer "fail increments the second number only" <|
+            \(x, y) ->
+              Expect.equal (x, y + 1) <| Score.fail (x, y)
+        , fuzz scoreFuzzer "toText converts both numbers to string and inserts a slash in between" <|
+            \(x, y) ->
+                let
+                    expected = (toString x) ++ "/" ++ (toString y)
+                    actual = Score.toText (x, y)
+                in
+                    Expect.equal expected actual
+        ]
+
+scoreFuzzer : Fuzzer Score
+scoreFuzzer =
+    tuple (int, int)
